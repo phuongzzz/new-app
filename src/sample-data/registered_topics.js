2c46e8b8e5b96d429fadc20e0c73c673cb0f8929
hueanmy <ltmai93@gmail.com>
2017-05-14 16:48:01 +0700

    add data register_topics

---
+++ src/sample-data/registered_topics.js
@@ -0,0 +1,27 @@
+module.exports = [
+    {
+        "id": "1",
+        "name": "Le Thi Mai",
+        "class": "LTU12A",
+        "title": "Ruby on Rails",
+        "compare": "100%",
+        "company": "Framgia"
+
+    },
+    {
+        "id": "2",
+        "name": "Tran Huu Nam Phuong",
+        "class": "LTU12A",
+        "title": "Python",
+        "compare": "100%",
+        "company": "Framgia"
+    },
+    {
+        "id": "3",
+        "name": "Nguyen Phu Cuong",
+        "class": "LTU12A",
+        "title": "PHP",
+        "compare": "100%",
+        "company": "Vnist"
+    }
+]
