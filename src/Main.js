b7838631bea6450dbfdfe517ab487e19dd890d2c
phuongzzz <yesterdayoncemore23@gmail.com>
2017-05-16 23:16:43 +0700

    add fading transition

--- src/Main.js
+++ src/Main.js
@@ -3,13 +3,23 @@ import NavigationBar from './components/common/NavigationBar';
 import Footer from './components/common/Footer';
 import './App.css';
 import '../node_modules/toastr/build/toastr.min.css';
+import { RouteTransition } from 'react-router-transition';
 
 class Main extends Component {
   render() {
     return (
       <div className="App">
         <NavigationBar {...this.props}/>
-        {React.cloneElement(this.props.children, this.props)}
+        <div className="content-wrapper">
+          <RouteTransition
+            pathname={this.props.location.pathname}
+            atEnter={{ opacity: 0.0 }}
+            atLeave={{ opacity: 0.4 }}
+            atActive={{ opacity: 1 }}
+          >
+            {React.cloneElement(this.props.children, this.props)}
+          </RouteTransition>
+        </div>
         <Footer/>
       </div>
     );
