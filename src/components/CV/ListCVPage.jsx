614a8fe5fca2e0f2e3ed6ff5a5d639bbc9150691
phuongzzz <yesterdayoncemore23@gmail.com>
2017-05-17 17:32:54 +0700

    small fix from phuong

--- src/components/CV/ListCVPage.jsx
+++ src/components/CV/ListCVPage.jsx
@@ -10,7 +10,7 @@ const ListCVPage = React.createClass({
         <div className="row">
           <h3 className="title">List CV</h3>
           <div className="row">
-            <Link to="/addnewcv"><button className="btn btn-default add-cv-btn">Add new</button></Link>
+            <Link to="/addnewcv"><button className="btn btn-success add-cv-btn">Add new</button></Link>
           </div>
           {this.props.cvs.map((cv, i) =>
             <div className="col-md-6" key={i}>
@@ -28,8 +28,6 @@ const ListCVPage = React.createClass({
                       <span>
                         <Link to={`/cv/${cv.id}`}><button type="button" className="btn btn-info"><span className="fa fa-eye"></span> View</button></Link>
                         <button type="button" className="btn btn-primary"><span className="fa fa-download"></span> Download</button>
-                        <button type="button" className="btn btn-success"><span className="fa fa-check-circle-o"></span> Approve
-                          </button>
                       </span>
                     </div>
                   </div>
