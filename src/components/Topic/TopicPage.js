22f72d752ff2f111b0bd424b70c671cc70603390
phuongzzz <yesterdayoncemore23@gmail.com>
2017-05-21 16:22:37 +0700

    save topic done

--- src/components/Topic/TopicPage.js
+++ src/components/Topic/TopicPage.js
@@ -4,14 +4,45 @@ import RegisteredTopic from './RegisteredTopic';
 import './topic-page.css';
 
 const TopicPage = React.createClass({
+
+  getInitialState() {
+    return {
+      search: '',
+    }
+  },
+
+  updateSearch(event) {
+    this.setState({search: event.target.value});
+  },
+
   render() {
+
+    let filteredTopics = this.props.topics.filter(
+      (topic) => {
+        return topic.title.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+      }
+    );
+
     return(
       <div>
-        <h1>This is topic page</h1>
+        <h1>Topics</h1>
+
+        <div className="row">
+          <div className="col-md-4 col-md-offset-4">
+            <input type="text" className="form-control" placeholder="Search topics by title (programing language)..."
+                   value={this.state.search}
+                   onChange={this.updateSearch}/>
+          </div>
+        </div>
+
         <div className="row">
           <h4>Available Topics</h4>
           <div className="topics col-md-10 col-md-offset-1 topic-wrap">
-            {this.props.topics.map((topic, i) => <Topic {...this.props} key={i} i={i} topic={topic} />)}
+            {filteredTopics.length !== 0 ?
+              filteredTopics.map((topic, i) =>
+                <Topic {...this.props} key={i} i={i} topic={topic} />) :
+                <div>Nothing found</div>
+            }
           </div>
         </div>
         <hr/>
